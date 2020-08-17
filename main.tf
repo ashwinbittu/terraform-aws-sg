b9b08d1d8a8e61cfcc94b541ff025f3a4b4425d6
ashwinbittu <ashwin.bittu@gmail.com>
2020-08-17 17:33:49 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,6 +48,14 @@ resource "aws_security_group" "elb-securitygroup" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+ ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  
   tags = {
     Name = "${var.app_name}-elb-security-group"
     environment  = var.app_env
