ab0b2a1e1eea6457d821397bb6738996c5ea1fc4
ashwinbittu <ashwin.bittu@gmail.com>
2022-05-24 08:10:36 +1000

    Delete examples directory

--- examples/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_security_group_instances_id" {
-  value = "${aws_security_group.myinstance.*.id}"
-}
-
-output "aws_security_group_elb_id" {
-  value = "${aws_security_group.elb-securitygroup.*.id}"
-}
