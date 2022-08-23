6e32730bad129561bdb316a847fa5ef18b4f6062
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-23 13:52:47 +1000

    Add files via upload

--- main.tf
+++ main.tf
@@ -6,45 +6,40 @@ resource "aws_security_group" "securitygroup" {
 
  }
 
-resource "aws_security_group_rule" "ingress_with_source_security_group_id" {
-    count = local.create ? length(var.ingress_with_source_security_group_id) : 0
-  
-    security_group_id = local.this_sg_id
-    type              = "ingress"
-  
-    source_security_group_id = var.ingress_with_source_security_group_id[count.index]["source_security_group_id"]
-    prefix_list_ids          = var.ingress_prefix_list_ids
-    description = lookup(
-      var.ingress_with_source_security_group_id[count.index],
-      "description",
-      "Ingress Rule",
-    )
-  
-    from_port = lookup(
-      var.ingress_with_source_security_group_id[count.index],
-      "from_port",
-      var.rules[lookup(
-        var.ingress_with_source_security_group_id[count.index],
-        "rule",
-        "_",
-      )][0],
-    )
-    to_port = lookup(
-      var.ingress_with_source_security_group_id[count.index],
-      "to_port",
-      var.rules[lookup(
-        var.ingress_with_source_security_group_id[count.index],
-        "rule",
-        "_",
-      )][1],
-    )
-    protocol = lookup(
-      var.ingress_with_source_security_group_id[count.index],
-      "protocol",
-      var.rules[lookup(
-        var.ingress_with_source_security_group_id[count.index],
-        "rule",
-        "_",
-      )][2],
-    )
-  }
+resource "aws_security_group_rule" "ingress_with_cidr_blocks" {
+  count = local.create ? length(var.ingress_with_cidr_blocks) : 0
+
+  security_group_id = local.this_sg_id
+  type              = "ingress"
+
+  cidr_blocks = split(
+    ",",
+    lookup(
+      var.ingress_with_cidr_blocks[count.index],
+      "cidr_blocks",
+      join(",", var.ingress_cidr_blocks),
+    ),
+  )
+  prefix_list_ids = var.ingress_prefix_list_ids
+  description = lookup(
+    var.ingress_with_cidr_blocks[count.index],
+    "description",
+    "Ingress Rule",
+  )
+
+  from_port = lookup(
+    var.ingress_with_cidr_blocks[count.index],
+    "from_port",
+    var.rules[lookup(var.ingress_with_cidr_blocks[count.index], "rule", "_")][0],
+  )
+  to_port = lookup(
+    var.ingress_with_cidr_blocks[count.index],
+    "to_port",
+    var.rules[lookup(var.ingress_with_cidr_blocks[count.index], "rule", "_")][1],
+  )
+  protocol = lookup(
+    var.ingress_with_cidr_blocks[count.index],
+    "protocol",
+    var.rules[lookup(var.ingress_with_cidr_blocks[count.index], "rule", "_")][2],
+  )
+}
--- variables.tf
+++ variables.tf
@@ -15,3 +15,9 @@ variable "aws_sg_name" {
 
 variable "aws_sg_description" {
 }
+
+variable "ingress_with_cidr_blocks" {
+  description = "List of ingress rules to create where 'cidr_blocks' is used"
+  type        = list(map(string))
+  default     = []
+}
