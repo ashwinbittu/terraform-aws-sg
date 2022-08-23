57b4535eb931c1006dbebce5885ad6a564a13eea
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-23 14:21:34 +1000

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "aws_security_group_instances_id" {
-  value = "${aws_security_group.myinstance.*.id}"
-}
-
-output "aws_security_group_elb_id" {
-  value = "${aws_security_group.elb-securitygroup.*.id}"
+  value = "${aws_security_group.securitygroup.*.id}"
 }
