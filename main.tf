9d99ee0a81944101e18ae3234b31de40c92fbe37
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-29 17:59:10 +1000

    Add files via upload

--- main.tf
+++ main.tf
@@ -93,6 +93,44 @@ resource "aws_security_group_rule" "computed_ingress_with_source_security_group_
   )
 }
 
+resource "aws_security_group_rule" "egress_with_cidr_blocks" {
+  count = local.create ? length(var.egress_with_cidr_blocks) : 0
+
+  security_group_id = local.this_sg_id
+  type              = "egress"
+
+  cidr_blocks = split(
+    ",",
+    lookup(
+      var.egress_with_cidr_blocks[count.index],
+      "cidr_blocks",
+      join(",", var.egress_cidr_blocks),
+    ),
+  )
+  prefix_list_ids = var.egress_prefix_list_ids
+  description = lookup(
+    var.egress_with_cidr_blocks[count.index],
+    "description",
+    "Egress Rule",
+  )
+
+  from_port = lookup(
+    var.egress_with_cidr_blocks[count.index],
+    "from_port",
+    var.rules[lookup(var.egress_with_cidr_blocks[count.index], "rule", "_")][0],
+  )
+  to_port = lookup(
+    var.egress_with_cidr_blocks[count.index],
+    "to_port",
+    var.rules[lookup(var.egress_with_cidr_blocks[count.index], "rule", "_")][1],
+  )
+  protocol = lookup(
+    var.egress_with_cidr_blocks[count.index],
+    "protocol",
+    var.rules[lookup(var.egress_with_cidr_blocks[count.index], "rule", "_")][2],
+  )
+}
+
 resource "aws_security_group_rule" "computed_egress_with_source_security_group_id" {
   count = local.create ? var.number_of_computed_egress_with_source_security_group_id : 0
 
